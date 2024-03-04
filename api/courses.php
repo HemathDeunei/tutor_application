8e907ab05a32f43899e573a170ec1f293785fba1
Hemath <hemath.deunei@gmail.com>
2024-03-04 10:12:07 +0530

    course api added

---
+++ api/courses.php
@@ -0,0 +1,84 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, 
+Access-Control-Request-Method, Access-Control-Allow-Origin");
+
+include "config.php";
+
+$RequestMethod = $_SERVER["REQUEST_METHOD"];
+
+if($RequestMethod == "POST"){
+    try {
+       
+        $Query      = "SELECT *, (SELECT name from pre_categories WHERE id = s.categories) AS category_name FROM pre_categories AS s WHERE s.is_parent = '0' AND s.status = '1' ORDER BY s.is_popular";
+
+        $Results    = mysqli_query($conn,$Query);
+        $ListArray  = array();
+
+        if($Results){
+            if (mysqli_num_rows($Results) > 0) 
+            {
+                while($record = mysqli_fetch_assoc($Results)) 
+                {
+                    $data = array();
+                    $data["id"]             = $record["id"];
+                    $data["name"]           = $record["name"];
+                    $data["code"]           = $record["code"];
+                    $data["image"]          = $record["image"];
+                    $data["categories"]     = $record["categories"];
+                    $data["category_name"]  = $record["category_name"];
+                    
+                    array_push($ListArray,$data);
+
+                }
+
+                $Data =[
+                    'status' => 200,
+                    'message' => 'Success',
+                    'data' => $ListArray,
+                ];
+            
+                header("HTTP/1.0 200 Success");
+                return json_encode($Data);
+            }else{
+                $Data =[
+                    'status' => 404,
+                    'message' => 'No Course Found'
+                ];
+            
+                header("HTTP/1.0 404 No Course Found");
+                return json_encode($Data);
+            }
+
+        }else{
+            $Data =[
+                'status' => 500,
+                'message' => 'Internal Server Error'
+            ];
+        
+            header("HTTP/1.0 500 Internal Server Error");
+            return json_encode($Data);
+        }
+
+    } catch (Exception $ex) {
+        $Data =[
+            'status' => 500,
+            'message' => 'Internal Server Error'
+        ];
+    
+        header("HTTP/1.0 500 Internal Server Error");
+        return json_encode($Data);
+    }
+}else{
+    $Data =[
+        'status' => 405,
+        'message' => $RequestMethod . ' Method Not Allowed'
+    ];
+
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($Data);
+}
+
+?>
