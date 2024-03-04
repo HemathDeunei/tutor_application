99e9554d91f0fabf2b1a19b7782c4deb20327799
Hemath <hemath.deunei@gmail.com>
2024-03-04 14:50:04 +0530

    get course tutor api updated

--- api/get_course_tutors.php
+++ api/get_course_tutors.php
@@ -55,11 +55,11 @@ if($RequestMethod == "POST"){
 
         }else{
             $Data =[
-                'status' => 500,
+                'status' => 404,
                 'message' => 'Internal Server Error'
             ];
         
-            header("HTTP/1.0 500 Internal Server Error");
+            header("HTTP/1.0 404 Internal Server Error");
             echo json_encode($Data);
         }
 
