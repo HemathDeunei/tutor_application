ab5c4eaa92b4971ce5d12bb7c459d86c274b0b83
Hemath <hemath.deunei@gmail.com>
2024-03-15 15:20:24 +0530

    API updates

--- api/update_profile.php
+++ api/update_profile.php
@@ -5,6 +5,10 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, 
 Access-Control-Request-Method, Access-Control-Allow-Origin");
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include "config.php";
 include "functions.php";
 
