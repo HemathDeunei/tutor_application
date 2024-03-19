424eeda3dc5d1a5b601614f194a285f8e97f3d27
Hemath <hemath.deunei@gmail.com>
2024-03-19 07:59:57 +0530

    Profile image api updated

--- api/update_profile_image.php
+++ api/update_profile_image.php
@@ -5,9 +5,9 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, 
 Access-Control-Request-Method, Access-Control-Allow-Origin");
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 include "config.php";
 include "functions.php";
@@ -21,60 +21,62 @@ if($RequestMethod == "POST")
 
        if(isset($_FILES["profile_image"]["tmp_name"]))
        {
-        echo "y";
-       }else{
-        echo "n";
-       }
+            $uploadDirectory    = PROFILE_UPLOAD_PATH . '/';
+            $uploadURL          = PROFILE;
+            $image_file_path    = "";
 
-       exit;
-
-        $uploadDirectory    = PROFILE_UPLOAD_PATH . '/';
-        $uploadURL          = PROFILE;
-        $image_file_path    = "";
-
-        if (!file_exists($uploadDirectory)) {
-            mkdir($uploadDirectory, 0755, true);
-        }
-
-        $file_ext  =    pathinfo($_FILES["profile_image"]['name'], PATHINFO_EXTENSION);
-        $file_name = $_FILES["profile_image"]["name"];
-        $file_tmp  = $_FILES["profile_image"]["tmp_name"];
-        $ext       = pathinfo($file_name, PATHINFO_EXTENSION);
-
-        if (in_array($ext, ['jpeg', 'jpg', 'png', 'gif','PNG', 'jfif'])) {
-            $newFileName = uniqid("profile_") . "." . $file_ext;
-            $uploadPath = $uploadDirectory . $newFileName;
-
-            if (move_uploaded_file($file_tmp, $uploadPath)) {
-                // $image_file_path= $uploadURL . $newFileName;
-                $image_file_path = $newFileName;
-
+            if (!file_exists($uploadDirectory)) {
+                mkdir($uploadDirectory, 0755, true);
             }
+
+            $file_ext  =    pathinfo($_FILES["profile_image"]['name'], PATHINFO_EXTENSION);
+            $file_name = $_FILES["profile_image"]["name"];
+            $file_tmp  = $_FILES["profile_image"]["tmp_name"];
+            $ext       = pathinfo($file_name, PATHINFO_EXTENSION);
+
+            if (in_array($ext, ['jpeg', 'jpg', 'png', 'gif','PNG', 'jfif'])) {
+                $newFileName = uniqid("profile_") . "." . $file_ext;
+                $uploadPath = $uploadDirectory . $newFileName;
+
+                if (move_uploaded_file($file_tmp, $uploadPath)) {
+                    // $image_file_path= $uploadURL . $newFileName;
+                    $image_file_path = $newFileName;
+
+                }
+            }
+
+            $UserArray                            = array();
+            $UserArray["photo"]                   = $image_file_path;
+        
+            $UpdatProfile = "UPDATE pre_users SET ";
+            foreach($UserArray as $k => $v)
+            {
+                $UpdatProfile .= $k."='". $v."', ";
+            }
+            $UpdatProfile = rtrim($UpdatProfile, ", ");
+            $UpdatProfile .= " where id = $user_id";
+
+            $ExecuteQuery = mysqli_query($conn,$UpdatProfile) or die ("Error in query: $UpdatProfile. ".mysqli_error($conn));
+
+            $Data =[
+                'status' => 200,
+                'message' => 'Profile Image Updated',
+                'profile_image' => $image_file_path
+            ];
+        
+            header("HTTP/1.0 200 Success");
+            echo json_encode($Data);
+
+        }else{
+            $Data =[
+                'status' => 200,
+                'message' => 'Profile Upload Failed',
+                'profile_image' => $image_file_path
+            ];
+        
+            header("HTTP/1.0 200 Upload Failed");
+            echo json_encode($Data);
         }
-
-    
-
-        $UserArray                            = array();
-        $UserArray["photo"]                   = $image_file_path;
-       
-
-        $UpdatProfile = "UPDATE pre_users SET ";
-        foreach($UserArray as $k => $v)
-        {
-            $UpdatProfile .= $k."='". $v."', ";
-        }
-        $UpdatProfile = rtrim($UpdatProfile, ", ");
-        $UpdatProfile .= " where id = $user_id";
-
-
-        $Data =[
-            'status' => 200,
-            'message' => 'Profile Image Updated',
-            'profile_image' => $image_file_path
-        ];
-    
-        header("HTTP/1.0 200 Success");
-        echo json_encode($Data);
 
     } catch (Exception $ex) {
         $Data =[
