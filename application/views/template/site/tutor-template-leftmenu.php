c9b6aec4c2c02ec289099322440c3860a129134c
Hemath <hemath.deunei@gmail.com>
2024-03-21 08:22:50 +0530

    Tutor profile API added

--- application/views/template/site/tutor-template-leftmenu.php
+++ application/views/template/site/tutor-template-leftmenu.php
@@ -83,53 +83,13 @@
 
 
 
-<div class="panel panel-default">
-	<div class="panel-heading">
-		<h4 class="panel-title">
-		<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapsePackages">
-            <i class="fa fa-archive"></i><?php echo get_languageword('Packages')?>
-		</a>
-	</h4>
-	</div>
-	<!--/.panel-heading -->
-	<div id="collapsePackages" class="panel-collapse <?php if(isset($activemenu) && $activemenu == 'Packages') echo 'collapse in'; else echo 'collapse';?>">
-		<div class="panel-body">
-			<ul class="dashboard-links">
-				<li <?php if(isset($activesubmenu) && $activesubmenu == 'list_packages') echo 'class="active"';?>><a href="<?php echo URL_TUTOR_LIST_PACKAGES;?>"><?php echo get_languageword('List Packages');?> </a></li>
-				<li <?php if(isset($activesubmenu) && $activesubmenu == 'mysubscriptions') echo 'class="active"';?>><a href="<?php echo base_url();?>tutor/mysubscriptions"><?php echo get_languageword('My Subscriptions');?> </a></li>
-			</ul>
-		</div>
-		<!--/.panel-body -->
-	</div>
-	<!--/.panel-collapse -->
-</div>
 
 
-<div class="panel panel-default">
-	<div class="panel-heading">
-		<h4 class="panel-title">
-		<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseReqs">
-            <i class="fa fa-money"></i><?php echo get_languageword('credit_Conversion')?><span class="hidden-xs"> <?php echo get_languageword('Request')?> </span>
-		</a>
-	</h4>
-	</div>
-	<!--/.panel-heading -->
-	<div id="collapseReqs" class="panel-collapse <?php if(isset($activemenu) && $activemenu == 'credit_conversion_requests') echo 'collapse in'; else echo 'collapse';?>">
-		<div class="panel-body">
-			<ul class="dashboard-links">
-				<li <?php if(isset($activesubmenu) && $activesubmenu == 'Pending') echo 'class="active"';?>><a href="<?php echo URL_TUTOR_CREDIT_CONVERSION_REQUESTS;?>/Pending"><?php echo get_languageword('Pending');?> </a></li>
-				<li <?php if(isset($activesubmenu) && $activesubmenu == 'Done') echo 'class="active"';?>><a href="<?php echo URL_TUTOR_CREDIT_CONVERSION_REQUESTS;?>/Done"><?php echo get_languageword('Done');?> </a></li>
-			</ul>
-		</div>
-		<!--/.panel-body -->
-	</div>
-	<!--/.panel-collapse -->
-</div>
 
 
 <div class="dashboard-link"><a <?php if(isset($activemenu) && $activemenu == 'user_credit_transactions') echo 'class="active"';?> href="<?php echo URL_TUTOR_CREDITS_TRANSACTION_HISTORY;?>"><i class="fa fa-exchange"></i><?php echo get_languageword('credits_Transactions');?><span class="hidden-xs"> <?php echo get_languageword('History')?> </span></a></div>
 
-<div class="dashboard-link"><a <?php if(isset($activemenu) && $activemenu == 'reviews') echo 'class="active"';?> href="<?php echo URL_TUTOR_USER_REVIEWS; ?>"><i class="fa fa-retweet"></i><?php echo get_languageword('Reviews');?></a></div>    
+
 
 <?php } ?>
 <!-- /.panel -->
